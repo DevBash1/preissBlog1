aefaa228b0b4a38709497b7098921c65c3740b70
AustineSamuel <austinesamuel914@gmail.com>
2021-07-19 07:55:13 -1000

    first blog page frontEnds checkout

---
+++ dataProcess.js
@@ -0,0 +1,181 @@
+
+//topBlogs LinkExample also add limit the like limit 30
+const topBlogs=[//all blog should be load like this from the server
+  {"title":"7 Ways To Save Battery Life Of Android Phone",
+"link":"#",
+"image":"battery.jpg",
+"admin":true,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"
+,//you should make simple table for comments
+"comments":[{
+  "name":"admin Example",
+  "image":"img4.png", //example
+  "content":"its cool"
+}]
+
+},
+
+{"title":" Top 10 Programming Languages to learn in 2021",
+"link":"#",
+"image":"tips.jpg",
+"admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"
+,//you should make simple table for comments
+"comments":[{
+  "name":"admin Example",
+  "image":"img4.png", //example
+  "content":"its cool"
+}]
+},
+
+{"title":"5 Best Free Keyword Research Tool For Bloggers 2021",
+"link":"#",
+"image":"img1.jpg",
+"admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"
+
+,//you should make simple table for comments
+"comments":[{
+  "name":"admin Example",
+  "image":"img4.png", //example
+  "content":"its cool"
+}]
+},
+
+{"title":"How To Make Money From Blogging In 2022",
+"link":"#",
+"image":"img2.jpg",
+"admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"
+,//you should make simple table for comments
+"comments":[{
+  "name":"admin Example",
+  "image":"img4.png", //example
+  "content":"its cool"
+}]
+},
+{"title":"Colour change facebook name account kaise banaye-full guide with working method",
+"link":"#",
+"image":"img3.jpg",
+"admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"
+,//you should make simple table for comments
+"comments":[{
+  "name":"admin Example",
+  "image":"img4.png", //example
+  "content":"its cool"
+}]
+},
+{"title":"Colour change facebook name account kaise banaye-full guide with working method",
+"link":"#",
+"image":"img3.jpg",
+"admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"
+,//you should make simple table for comments
+"comments":[{
+  "name":"admin Example",
+  "image":"img4.png", //example
+  "content":"its cool"
+}]
+},
+{"title":"Colour change facebook name account kaise banaye-full guide with working method",
+"link":"#",
+"image":"img3.jpg",
+"admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"
+,//you should make simple table for comments
+"comments":[{
+  "name":"admin Example",
+  "image":"img4.png", //example
+  "content":"its cool"
+}]
+},
+{"title":"Colour change facebook name account kaise banaye-full guide with working method",
+"link":"#",
+"image":"img3.jpg",
+"admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"
+,//you should make simple table for comments
+"comments":[{
+  "name":"admin Example",
+  "image":"img4.png", //example
+  "content":"its cool"
+}]
+},
+{"title":"Colour change facebook name account kaise banaye-full guide with working method",
+"link":"#",
+"image":"img3.jpg",
+"admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"
+,//you should make simple table for comments
+"comments":[{
+  "name":"admin Example",
+  "image":"img4.png", //example
+  "content":"its cool"
+}]
+},
+];//its example
+
--- main.js
+++ main.js
@@ -38,118 +38,16 @@ $("nav #menu").click(function(){
 $("#menuContent").fadeOut(0);
 $("#menuContent #main").css("width","0")
 }//menu ui function ends
-
-//topBlogs LinkExample also add limit the like limit 30
-const topBlogs=[//all blog should be load like this from the server
-  {"title":"7 Ways To Save Battery Life Of Android Phone",
-"link":"#",
-"image":"battery.jpg",
-"admin":true,
-"blogContent":`
-5 Essential Tips For All Smartphone Users
-Admin July 14, 2021
-
-The most popular mobile operating system of the present time is Android.  Its p…
-`,//this should be the blog content in text format admin can update in textarea
-
-"date":"july - 2020"
-,//you should make simple table for comments
-"comments":[{
-  "name":"admin Example",
-  "image":"img4.png", //example
-  "content":"its cool"
-}]
-
-},
-
-{"title":" Top 10 Programming Languages to learn in 2021",
-"link":"#",
-"image":"tips.jpg",
-"admin":false,
-"blogContent":`
-5 Essential Tips For All Smartphone Users
-Admin July 14, 2021
-
-The most popular mobile operating system of the present time is Android.  Its p…
-`,//this should be the blog content in text format admin can update in textarea
-
-"date":"july - 2020"
-,//you should make simple table for comments
-"comments":[{
-  "name":"admin Example",
-  "image":"img4.png", //example
-  "content":"its cool"
-}]
-},
-
-{"title":"5 Best Free Keyword Research Tool For Bloggers 2021",
-"link":"#",
-"image":"img1.jpg",
-"admin":false,
-"blogContent":`
-5 Essential Tips For All Smartphone Users
-Admin July 14, 2021
-
-The most popular mobile operating system of the present time is Android.  Its p…
-`,//this should be the blog content in text format admin can update in textarea
-
-"date":"july - 2020"
-
-,//you should make simple table for comments
-"comments":[{
-  "name":"admin Example",
-  "image":"img4.png", //example
-  "content":"its cool"
-}]
-},
-
-{"title":"How To Make Money From Blogging In 2022",
-"link":"#",
-"image":"img2.jpg",
-"admin":false,
-"blogContent":`
-5 Essential Tips For All Smartphone Users
-Admin July 14, 2021
-
-The most popular mobile operating system of the present time is Android.  Its p…
-`,//this should be the blog content in text format admin can update in textarea
-
-"date":"july - 2020"
-,//you should make simple table for comments
-"comments":[{
-  "name":"admin Example",
-  "image":"img4.png", //example
-  "content":"its cool"
-}]
-},
-{"title":"Colour change facebook name account kaise banaye-full guide with working method",
-"link":"#",
-"image":"img3.jpg",
-"admin":false,
-"blogContent":`
-5 Essential Tips For All Smartphone Users
-Admin July 14, 2021
-
-The most popular mobile operating system of the present time is Android.  Its p…
-`,//this should be the blog content in text format admin can update in textarea
-
-"date":"july - 2020"
-,//you should make simple table for comments
-"comments":[{
-  "name":"admin Example",
-  "image":"img4.png", //example
-  "content":"its cool"
-}]
-}
-];//its example
-
 function showBlogs(arr=topBlogs){
   $("#topBlogs").html("");
+  if(arr.length>5){
+    arr=arr.slice(0,5);
+  }
   arr.forEach((e,i) => {
     
   $("#topBlogs").append(`
 <div id="blogGroup"  style="background-image : url('images/${e.image}');"  index="${i}" class="flexBottom" onclick="href('${e.link}')">
-<div class="flexBottom w3-padding w3-block" style="flex-wrap:wrap; width:100%; height:100%;background:linear-gradient(rgb(255, 255, 255),rgba(0, 0, 0, 0.6))">
+<div class="flexBottom w3-padding w3-block" style="flex-wrap:wrap; width:100%; height:100%;background:linear-gradient(rgba(255, 255, 255,0.3),rgba(0, 0, 0, 0.6))">
 <h2 class="w3-center w3-bold  w3-block" style="padding:0;"><br>
     ${e.title.length>50 ? e.title.slice(0,50)+"...":e.title}
 </h2>
@@ -172,16 +70,18 @@ function showBlogsList(arr=topBlogs){//this function display the blog list almos
     $("#mainListContent").append(`
 
   <div index="${i}" onclick="href('${e.link}')"  id="blogLink"class="flexBetween w3-block">
-    <img src="images/${e.image}">
+    <img id="pcOnly" src="images/${e.image}">
     <div id="blogLinkInfo" class="w3-container">
 <h1>
  ${e.title}
   </h1>
 <div id="date"><i class="fa fa-clock"> ${e.date}</i> ${ e.admin=="true" || e.admin ? " <i class='fa fa-user-o'></i> admin":"" }
-  </div>
+<img id="mobileOnly" style="position:relative !important"  src="images/${e.image}">
+  
+</div>
 <div style="color:dimgrey;">
 ${e.blogContent.length>50 ? e.blogContent.slice(0,50)+"....":e.blogContent} 
-</div>
+</div>   
     </div>
   </div>
 `)
@@ -218,5 +118,8 @@ showAndHideMenu();
 showBlogs();
 showBlogsList();
 showSomeComments();
+setTimeout(() => {
+  $("html,body,#bodyM").animate({scrollTop:5000},"fast");
+},100);
 }//end onload
 
