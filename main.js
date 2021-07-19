4748a7624b9e41a59083838bb6aa8f05643817da
AustineSamuel <austinesamuel914@gmail.com>
2021-07-19 03:52:10 -1000

    blog list added

--- main.js
+++ main.js
@@ -39,33 +39,72 @@ $("#menuContent").fadeOut(0);
 $("#menuContent #main").css("width","0")
 }//menu ui function ends
 
-//topBlogs LinkExample
-const topBlogs=[
+//topBlogs LinkExample also add limit the like limit 30
+const topBlogs=[//all blog should be load like this from the server
   {"title":"7 Ways To Save Battery Life Of Android Phone",
 "link":"#",
-"image":"image/battery.jpg",
+"image":"battery.jpg",
 "admin":true,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
 "date":"july - 2020"
 },
 
-{"title":"7 Ways To Save Battery Life Of Android Phone",
+{"title":" Top 10 Programming Languages to learn in 2021",
 "link":"#",
-"image":"image/b1.jpg",
+"image":"tips.jpg",
 "admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
 "date":"july - 2020"},
 
-{"title":"7 Ways To Save Battery Life Of Android Phone",
+{"title":"5 Best Free Keyword Research Tool For Bloggers 2021",
 "link":"#",
-"image":"image/b1.jpg",
+"image":"img1.jpg",
 "admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
 "date":"july - 2020"},
 
-{"title":"7 Ways To Save Battery Life Of Android Phone",
+{"title":"How To Make Money From Blogging In 2022",
 "link":"#",
-"image":"image/b1.jpg",
+"image":"img2.jpg",
 "admin":false,
-"date":"july - 2020"},
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
 
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"},
+{"title":"Colour change facebook name account kaise banaye-full guide with working method",
+"link":"#",
+"image":"img3.jpg",
+"admin":false,
+"blogContent":`
+5 Essential Tips For All Smartphone Users
+Admin July 14, 2021
+
+The most popular mobile operating system of the present time is Android.  Its p…
+`,//this should be the blog content in text format admin can update in textarea
+
+"date":"july - 2020"}
 ];//its example
 
 function showBlogs(arr=topBlogs){
@@ -73,10 +112,10 @@ function showBlogs(arr=topBlogs){
   arr.forEach((e,i) => {
     
   $("#topBlogs").append(`
-<div id="blogGroup"  style="background-image : url(''${e.image});"  index="${i}" class="flexBottom" onclick="href('${e.link}')">
+<div id="blogGroup"  style="background-image : url('images/${e.image}');"  index="${i}" class="flexBottom" onclick="href('${e.link}')">
 <div class="flexBottom w3-padding w3-block" style="flex-wrap:wrap; width:100%; height:100%;background:linear-gradient(rgb(255, 255, 255),rgba(0, 0, 0, 0.6))">
-<h2 class="w3-center w3-bold  w3-block" style="padding:0;"><br><br>
-    ${e.title}
+<h2 class="w3-center w3-bold  w3-block" style="padding:0;"><br>
+    ${e.title.length>50 ? e.title.slice(0,50)+"...":e.title}
 </h2>
 <div class="flexStart w3-block">
 <button class="fa fa-user-o" style='display:${e.admin=="true" || e.admin ? "":"none"}'> admin</button>
@@ -87,10 +126,37 @@ function showBlogs(arr=topBlogs){
   `);
 });
 
+}//show  blog ends
+
+
+function showBlogsList(arr=topBlogs){//this function display the blog list almost all
+  $("#mainListContent").html("");
+  arr.forEach((e,i)=>{
+   
+    $("#mainListContent").append(`
+
+  <div index="${i}" onclick="href('${e.link}')"  id="blogLink"class="flexBetween w3-block">
+    <img src="images/${e.image}">
+    <div id="blogLinkInfo" class="w3-container">
+<h1>
+ ${e.title}
+  </h1>
+<div id="date"><i class="fa fa-clock"> ${e.date}</i> ${ e.admin=="true" || e.admin ? " <i class='fa fa-user-o'></i> admin":"" }
+  </div>
+<div style="color:dimgrey;">
+${e.blogContent.length>50 ? e.blogContent.slice(0,50)+"....":e.blogContent} 
+</div>
+    </div>
+  </div>
+`)
+});
+
+
 }
 
 showAndHideSearch();
 showAndHideMenu();
 showBlogs();
+showBlogsList();
 }//end onload
 
